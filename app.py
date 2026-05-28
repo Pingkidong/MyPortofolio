161a653544f79a7203f1cff34275fc80cce40c10
Silvia Naada Kamilia <135617601+Pingkidong@users.noreply.github.com>
2026-05-28 08:25:23 +0700

    Add Welmart page to navigation
    adding new project

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(
 )
 
 home = st.Page("pages/home.py", title="Home", icon="🏠", default=True)
+welmart  = st.Page("pages/decline_sales.py", title="Welmart", icon="📊")
 car  = st.Page("pages/car_evaluation.py", title="Car Evaluation", icon="🚗")
-
-pg = st.navigation([home, car], position="hidden")
+pg = st.navigation([home, welmart, car], position="hidden")
 pg.run()
